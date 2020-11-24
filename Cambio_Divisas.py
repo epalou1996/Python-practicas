c59f8de6e8e19e5d8e79a6493fd76e87dd8e231d
Eduardo Palou <eduardopalou96@gmail.com>
2020-11-24 15:06:34 -0500

    Cambios de calidad para mejor uso del programa

--- Cambio_Divisas.py
+++ Cambio_Divisas.py
@@ -2,17 +2,26 @@
 
 #First we stablish the trade values in a dictionary, they will be usd based(as everything is)
 
-dic = {"COP": 3644,
+'''dic = {"COP": 3644,
 "EURO": 0.84,
 "BTC" : 0.000052 ,
 "MXN": 20.02 ,
 "GBP": 0.75
-}
+}'''
+
+
+#We can also set the dic in a new file, and bring it with python, making it easier to add new currencies
+from Divisas import dic
+currencies="USD, "
+for key, value in dic.items():
+	currencies+= key + ", "
+
+currencies= currencies[:-2] +". "
 
 
 # The 3 inputs we need for the process
-current=input("Ingrese su moneda, COP USD EURO BTC MXN GBP en mayusculas: ")
-new = input("Ingrese a la moneda que desea convertir, COP USD EURO BTC MXN GBP en mayusculas: ")
+current=input("Ingrese su moneda: "+ currencies +" en mayusculas: ")
+new = input("Ingrese a la moneda que desea convertir: "+ currencies +" en mayusculas: ")
 quantity = float(input("Ingrese la cantidad: "))
 
 # As the base coin is usd, if the input coin is usd, we only need to multiply te input value for its exchange rate, if anything else, we need to also 
